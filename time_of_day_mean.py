32cde71dd5306ac2edaf941c1778ef2c023312e8
Indivicivet <indivicivet@gmail.com>
2022-08-17 23:13:22 +0100

    include counts at each time

--- time_of_day_mean.py
+++ time_of_day_mean.py
@@ -10,16 +10,22 @@ def show_average_rating_by_time(data, out_file=None):
     for checkin in data:
         category_data[checkin.datetime.hour][checkin.rating or 0] += 1
 
+    counts = {
+        cat: sum(rating_counts.values())
+        for cat, rating_counts in category_data.items()
+    }
     y_data = {
         cat: (
             sum(rating * count for rating, count in rating_counts.items())
-            / sum(rating_counts.values())
+            / counts[cat]
         )
         for cat, rating_counts in category_data.items()
     }
     x_data = range(24)
-    plt.plot(x_data, [y_data.get(x, 0) for x in x_data])
-    #plt.legend()
+    plt.plot(x_data, [y_data.get(x, 0) for x in x_data], label="average rating")
+    # handpicked n:
+    plt.plot(x_data, [counts.get(x, 0) / 200 for x in x_data], label="checkins(/200)")
+    plt.legend()
     if out_file is None:
         plt.show()
     else:
