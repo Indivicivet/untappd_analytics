ec87cfdacd4bc1cb4222b73c1ce9ea74c728b0c4
Indivicivet <indivicivet@gmail.com>
2022-08-17 23:00:21 +0100

    sort the categories for better hourz

--- ratings_by_various.py
+++ ratings_by_various.py
@@ -13,7 +13,7 @@ def show_histogram(data, func=None, out_file=None):
         category_data[func(checkin)][checkin.rating or 0] += 1
 
     x_data = [i / 4 for i in range(1, 21)]
-    for label, y_data in category_data.items():
+    for label, y_data in sorted(category_data.items()):
         plt.plot(x_data, [y_data.get(x, 0) for x in x_data], label=label)
     plt.legend()
     if out_file is None:
