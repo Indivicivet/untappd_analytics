b363409d5ea7759ab78d8099856f455f4b0bfeba
Indivicivet <indivicivet@gmail.com>
2022-08-17 22:55:41 +0100

    used nested defaultdicts for category histogram

--- ratings_by_various.py
+++ ratings_by_various.py
@@ -6,14 +6,13 @@ import untappd
 
 
 def show_category_histogram(data):
-    category_data = defaultdict(lambda: [0] * 20)
+    category_data = defaultdict(lambda: defaultdict(int))
     for checkin in data:
-        rating_n = round((checkin.rating or 0) * 4)
-        category_data[checkin.beer.get_style_category()][rating_n - 1] += 1
+        category_data[checkin.beer.get_style_category()][checkin.rating or 0] += 1
 
     x_data = [i / 4 for i in range(1, 21)]
     for label, y_data in category_data.items():
-        plt.plot(x_data, y_data, label=label)
+        plt.plot(x_data, [y_data.get(x, 0) for x in x_data], label=label)
     plt.legend()
     plt.show()
 
