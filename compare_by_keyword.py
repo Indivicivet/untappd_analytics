d1266e55ed67f50c3b4cbe71fc6e5faeeabde0ad
Indivicivet <indivicivet@gmail.com>
2022-08-23 16:54:39 +0100

    print average rating by keyword

--- compare_by_keyword.py
+++ compare_by_keyword.py
@@ -27,15 +27,25 @@ HOPS = [
     
 
 XS = [i / 4 for i in range(1, 21)]
+scores = []
 
-for keyword in FRUIT:
+for keyword in HOPS:
     # look for anywhere in checkin; probably simplest
     # (could just look in beer name and comment)
     counts = Counter(c.rating for c in CHECKINS if keyword in str(c).lower())
+    total = sum(counts.values())
+    scores.append((
+        sum(r * n for r, n in counts.items()) / total,
+        keyword,
+        total,
+    ))
     plt.plot(
         *untappd_utils.smooth_ratings(XS, [counts.get(x, 0) for x in XS]),
         label=keyword,
     )
+
+for rating, keyword, count in sorted(scores, reverse=True):
+    print(f"{rating: .2f} {keyword}  ({count})")
 
 plt.legend()
 plt.show()
