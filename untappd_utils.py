7238c30a0043d21602ab21dcaaef7740d053e95d
Indivicivet <indivicivet@gmail.com>
2022-08-23 16:29:41 +0100

    add customizable smoothing factor

--- untappd_utils.py
+++ untappd_utils.py
@@ -3,7 +3,11 @@ import numpy as np
 from scipy import interpolate, ndimage
 
 
-def smooth_ratings(x0, y0, samples=200):
+def smooth_ratings(x0, y0, samples=200, amount=0.1):
     x_smooth = np.linspace(min(x0), max(x0), samples, endpoint=True)
     y_linear = interpolate.interp1d(x0, y0)(x_smooth)
-    return x_smooth, ndimage.gaussian_filter(y_linear, samples * 0.01)
+    y_smooth = ndimage.gaussian_filter(
+        y_linear,
+        samples * amount / (max(x0) - min(x0)),
+    )
+    return x_smooth, y_smooth
