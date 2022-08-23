16ea60b37bc4335811346c51831f71eee2b0270b
Indivicivet <indivicivet@gmail.com>
2022-08-23 16:36:59 +0100

    update ratings over time to use checkins (and also not be broken)

--- ratings_over_time.py
+++ ratings_over_time.py
@@ -3,10 +3,10 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-from untappd import load_latest_json
+import untappd
 
 print("loading")
-data = load_latest_json()
+CHECKINS = untappd.load_latest_checkins()
 
 CUMULATIVE = False
 
@@ -17,26 +17,26 @@ frames = []
 slices = (
     [
         slice(0, pos)
-        for pos in range(STEP, len(data) - STEP, STEP)
+        for pos in range(STEP, len(CHECKINS) - STEP, STEP)
     ]
     if CUMULATIVE else
     [
         slice(start, start + SPREAD)
-        for start in range(0, len(data) - SPREAD, STEP)
+        for start in range(0, len(CHECKINS) - SPREAD, STEP)
     ]
 )
     
 for i, ci_slice in enumerate(slices):
-    checkins = data[ci_slice]
+    checkins = CHECKINS[ci_slice]
     frames.append(
         {
             "num_ratings": len(checkins),
             "ratings": Counter(
-                round(float(ci["rating_score"] or "0") * 4)
+                round(float(ci.rating or 0) * 4)
                 for ci in checkins
             ),
-            "start_date": checkins[0]["created_at"].split()[0],  # date only
-            "end_date": checkins[-1]["created_at"].split()[0],
+            "start_date": checkins[0].datetime.date(),  # date only
+            "end_date": checkins[-1].datetime.date(),
         }
     )
 
@@ -51,7 +51,7 @@ def init():
     return ln,
 
 def update(frame):
-    ax.set_title(frame["start_date"] + " ~ " + frame["end_date"])
+    ax.set_title(f'{frame["start_date"]} ~ {frame["end_date"]}')
     ax.set_ylim(0, frame["num_ratings"] * (0.3 if CUMULATIVE else 0.4))
     ln.set_data(
         x_data,
