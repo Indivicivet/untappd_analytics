530fc0f3594fa8f5c599194faba8bc733538165a
Indivicivet <indivicivet@gmail.com>
2022-08-23 16:25:22 +0100

    gaussian filter plots in compare_by_keyword.py

--- compare_by_keyword.py
+++ compare_by_keyword.py
@@ -1,6 +1,8 @@
 from collections import Counter
 
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import interpolate, ndimage
 
 import untappd
 
@@ -16,13 +18,23 @@ KEYWORDS = [
     # "guava",
 ]
 
+
+def smooth_ratings(x0, y0, samples=200):
+    x_smooth = np.linspace(min(x0), max(x0), samples, endpoint=True)
+    y_linear = interpolate.interp1d(x0, y0)(x_smooth)
+    return x_smooth, ndimage.gaussian_filter(y_linear, samples * 0.01)
+    
+
 XS = [i / 4 for i in range(1, 21)]
 
 for keyword in KEYWORDS:
     # look for anywhere in checkin; probably simplest
     # (could just look in beer name and comment)
     counts = Counter(c.rating for c in CHECKINS if keyword in str(c).lower())
-    plt.plot(XS, [counts.get(x, 0) for x in XS], label=keyword)
+    plt.plot(
+        *smooth_ratings(XS, [counts.get(x, 0) for x in XS]),
+        label=keyword,
+    )
 
 plt.legend()
 plt.show()
