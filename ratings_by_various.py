3c22d9eedde3beba1ecd5cf6f50e62d96d928b21
Indivicivet <indivicivet@gmail.com>
2022-08-23 16:33:29 +0100

    smooth in ratings_by_various.py

--- ratings_by_various.py
+++ ratings_by_various.py
@@ -4,6 +4,7 @@ import datetime
 import matplotlib.pyplot as plt
 
 import untappd
+import untappd_utils
 
 
 class SessionTracker:
@@ -27,8 +28,9 @@ def show_histogram(data, func, out_file=None):
         category_data[func(checkin)][checkin.rating or 0] += 1
 
     x_data = [i / 4 for i in range(1, 21)]
-    for label, y_data in sorted(category_data.items()):
-        plt.plot(x_data, [y_data.get(x, 0) for x in x_data], label=label)
+    for label, counts in sorted(category_data.items()):
+        y_data = [counts.get(x, 0) for x in x_data]
+        plt.plot(*untappd_utils.smooth_ratings(x_data, y_data), label=label)
     plt.legend()
     if out_file is None:
         plt.show()
