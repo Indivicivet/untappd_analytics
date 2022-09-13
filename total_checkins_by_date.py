17758a22f49fc788af3bfa8d6615c69a9eaad63e
Indivicivet <indivicivet@gmail.com>
2022-09-14 00:57:03 +0100

    seaborn.set() in total_checkins_by_date

--- total_checkins_by_date.py
+++ total_checkins_by_date.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
+import seaborn
 
 import untappd
 
 CHECKINS = untappd.load_latest_checkins()
 
 times = [checkin.datetime for checkin in CHECKINS]
+
+seaborn.set()
 
 plt.figure(figsize=(12.8, 7.2))
 plt.plot(times, range(len(times)))
