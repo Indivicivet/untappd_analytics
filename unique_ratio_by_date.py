6ef9901df8069b969962d99639fe83a336e4381c
Indivicivet <indivicivet@gmail.com>
2022-09-20 13:46:35 +0100

    add moving average plot of uniqueness ratio

--- unique_ratio_by_date.py
+++ unique_ratio_by_date.py
@@ -15,11 +15,27 @@ for i, checkin in enumerate(CHECKINS):
         n_unique += 1
     proportion_uniques.append(n_unique / (i + 1))
 
+RECENCY_SIZE = 200
+recent_uniques = [
+    # ew :)
+    (prop_b * (i + RECENCY_SIZE + 1) - prop_a * (i + 1)) / RECENCY_SIZE
+    for i, (prop_a, prop_b) in enumerate(zip(
+        proportion_uniques,
+        proportion_uniques[RECENCY_SIZE:],
+    ))
+]
+
 seaborn.set()
 
 plt.figure(figsize=(12.8, 7.2))
-plt.plot(times, proportion_uniques, label="proportion unique")
+plt.plot(times, proportion_uniques, label="cumulative")
+plt.plot(
+    times[RECENCY_SIZE//2:-RECENCY_SIZE//2],  # must be even ^^;
+    recent_uniques,
+    label=f"moving average {RECENCY_SIZE}",
+)
 plt.ylim(0, 1)
 plt.xlabel("date")
 plt.ylabel("unique ratio")
+plt.legend()
 plt.show()
