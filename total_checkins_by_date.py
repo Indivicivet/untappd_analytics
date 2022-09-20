68e1dab277081728f069443075a7cc098d4979cc
Indivicivet <indivicivet@gmail.com>
2022-09-20 14:31:24 +0100

    plot moving average proportion unique by date in total_checkins_by_date.py

--- total_checkins_by_date.py
+++ total_checkins_by_date.py
@@ -6,11 +6,20 @@ import untappd
 CHECKINS = untappd.load_latest_checkins()
 
 times = [checkin.datetime for checkin in CHECKINS]
+proportion_uniques = []
+n_unique = 0
+unique_beers = set()
+for i, checkin in enumerate(CHECKINS):
+    if checkin.beer not in unique_beers:
+        unique_beers.add(checkin.beer)
+        n_unique += 1
+    proportion_uniques.append(n_unique / (i + 1))
 
 seaborn.set()
 
 plt.figure(figsize=(12.8, 7.2))
-plt.plot(times, range(1, len(times) + 1))
+plt.plot(times, proportion_uniques, label="proportion unique")
+plt.ylim(0, 1)
 plt.xlabel("date")
-plt.ylabel("total checkins")
+plt.ylabel("unique ratio")
 plt.show()
