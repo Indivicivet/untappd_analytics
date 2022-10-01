085d9069191af5a348589cfe5540c2d6f4021a6b
Indivicivet <indivicivet@gmail.com>
2022-10-01 23:43:06 +0100

    change total checkins by date to show... that. (essentially a revert, I think)

--- total_checkins_by_date.py
+++ total_checkins_by_date.py
@@ -6,20 +6,11 @@ import untappd
 CHECKINS = untappd.load_latest_checkins()
 
 times = [checkin.datetime for checkin in CHECKINS]
-proportion_uniques = []
-n_unique = 0
-unique_beers = set()
-for i, checkin in enumerate(CHECKINS):
-    if checkin.beer not in unique_beers:
-        unique_beers.add(checkin.beer)
-        n_unique += 1
-    proportion_uniques.append(n_unique / (i + 1))
 
 seaborn.set()
 
 plt.figure(figsize=(12.8, 7.2))
-plt.plot(times, proportion_uniques, label="proportion unique")
-plt.ylim(0, 1)
+plt.plot(times, range(len(CHECKINS)), label="total checkins")
 plt.xlabel("date")
-plt.ylabel("unique ratio")
+plt.ylabel("checkins")
 plt.show()
