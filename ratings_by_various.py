eeb651d75b148d96dd6405dca95b4d1ba66d806c
Indivicivet <indivicivet@gmail.com>
2022-10-27 11:59:39 +0100

    specify title when saving plots

--- ratings_by_various.py
+++ ratings_by_various.py
@@ -160,6 +160,7 @@ def save_various_plots(checkins, out_dir=None):
             checkins,
             func=func,
             normalize=tag not in ["hour"],
+            title=f"ratings by {tag}",
             out_file=out_file,
         )
         print(f"saved ratings by {tag} to {out_file}")
