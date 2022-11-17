2ee7996be3400e09071de3f717f3669986aa1b66
Indivicivet <indivicivet@gmail.com>
2022-11-17 01:12:52 +0000

    add abv_by_date

---
+++ abv_by_date.py
@@ -0,0 +1,38 @@
+import datetime
+import statistics
+
+import matplotlib.pyplot as plt
+import seaborn
+
+import untappd
+
+CHECKINS = untappd.load_latest_checkins()
+
+# todo :: much of this code is copied from category_by_date
+# once I have nice smoother plots, ideally move functionality
+# into a shared file
+
+GROUP_TIMESPAN = datetime.timedelta(days=31 * 3)
+start_date = min(c.datetime for c in CHECKINS)
+end_date = max(c.datetime for c in CHECKINS) - GROUP_TIMESPAN
+day_starts = [
+    datetime.datetime.fromordinal(n)
+    for n in range(start_date.toordinal(), end_date.toordinal())
+]
+
+abvs = [
+    statistics.mean([
+        ci.beer.abv
+        for ci in CHECKINS
+        if start <= ci.datetime < start + GROUP_TIMESPAN
+    ])
+    for start in day_starts
+]
+
+seaborn.set()
+
+plt.figure(figsize=(12.8, 7.2))
+plt.plot(day_starts, abvs)
+plt.xlabel("start date")
+plt.ylabel("average abv")
+plt.show()
