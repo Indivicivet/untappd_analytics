a5f796d9e03831e2bb4f2a0f328d8562308c4c5c
Indivicivet <indivicivet@gmail.com>
2022-11-17 01:04:03 +0000

    update category_by_date to be a (not-so-smooth) plot of categories over time

--- category_by_date.py
+++ category_by_date.py
@@ -1,3 +1,6 @@
+import datetime
+from collections import defaultdict, Counter
+
 import matplotlib.pyplot as plt
 import seaborn
 
@@ -5,17 +8,31 @@ import untappd
 
 CHECKINS = untappd.load_latest_checkins()
 
+GROUP_TIMESPAN = datetime.timedelta(days=31 * 4)
+start_date = min(c.datetime for c in CHECKINS)
+end_date = max(c.datetime for c in CHECKINS) - GROUP_TIMESPAN
+day_starts = [
+    datetime.datetime.fromordinal(n)
+    for n in range(start_date.toordinal(), end_date.toordinal())
+]
+
+category_counts = []
+for start in day_starts:
+    counts_this_time = Counter()
+    for ci in CHECKINS:
+        if start <= ci.datetime < start + GROUP_TIMESPAN:
+            counts_this_time[ci.beer.get_style_category()] += 1
+    category_counts.append(counts_this_time)
+
 seaborn.set()
 
-# todo :: would much rather <checkins in period> rather than cumulative
 plt.figure(figsize=(12.8, 7.2))
 for category in untappd.CATEGORY_KEYWORDS:
-    results = [
-        checkin.datetime
-        for checkin in CHECKINS
-        if checkin.beer.get_style_category() == category
-    ]
-    plt.plot(results, range(len(results)), label=category)
+    plt.plot(
+        day_starts,
+        [counts[category] for counts in category_counts],
+        label=category,
+    )
 plt.xlabel("date")
 plt.ylabel("checkins")
 plt.legend()
