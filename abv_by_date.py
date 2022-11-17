39b860ed803edeb5bf6e9d3f99f8f41f50d1da61
Indivicivet <indivicivet@gmail.com>
2022-11-17 01:17:03 +0000

    add average ibu as option to abv_by_date

--- abv_by_date.py
+++ abv_by_date.py
@@ -12,6 +12,8 @@ CHECKINS = untappd.load_latest_checkins()
 # once I have nice smoother plots, ideally move functionality
 # into a shared file
 
+SHOW_IBUS = False
+
 GROUP_TIMESPAN = datetime.timedelta(days=31 * 3)
 start_date = min(c.datetime for c in CHECKINS)
 end_date = max(c.datetime for c in CHECKINS) - GROUP_TIMESPAN
@@ -33,6 +35,24 @@ seaborn.set()
 
 plt.figure(figsize=(12.8, 7.2))
 plt.plot(day_starts, abvs)
+
+if SHOW_IBUS:
+    # todo :: should have separate axes + legend
+    # (then probably don't need a flag :D)
+    ibus = [
+        0.1  # hacky :)
+        * statistics.mean([
+            ci.beer.ibu
+            for ci in CHECKINS
+            if (
+                    start <= ci.datetime < start + GROUP_TIMESPAN
+                    and ci.beer.ibu > 0
+            )
+        ])
+        for start in day_starts
+    ]
+    plt.plot(day_starts, ibus)
+
 plt.xlabel("start date")
 plt.ylabel("average abv")
 plt.show()
