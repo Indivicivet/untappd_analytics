6b7f83198517d92ea1f221ddbfdaa508f4fc5586
Indivicivet <indivicivet@gmail.com>
2022-11-17 00:23:07 +0000

    add (dashed) non-taster checkins to total_checkins_by_date

--- total_checkins_by_date.py
+++ total_checkins_by_date.py
@@ -6,11 +6,23 @@ import untappd
 CHECKINS = untappd.load_latest_checkins()
 
 times = [checkin.datetime for checkin in CHECKINS]
+times_non_taster = [
+    checkin.datetime
+    for checkin in CHECKINS
+    if checkin.serving_type != "Taster"
+]
 
 seaborn.set()
 
 plt.figure(figsize=(12.8, 7.2))
-plt.plot(times, range(len(CHECKINS)), label="total checkins")
+plt.plot(
+    times_non_taster,
+    range(len(times_non_taster)),
+    label="non-taster",
+    linestyle="dashed"
+)
+plt.plot(times, range(len(times)), label="total checkins")
 plt.xlabel("date")
 plt.ylabel("checkins")
+plt.legend()
 plt.show()
