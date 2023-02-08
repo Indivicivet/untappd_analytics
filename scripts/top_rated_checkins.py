4ff8c8f9fc677fcb5d3f4a51073c84f90bc85d12
indi <indivicivet@gmail.com>
2023-02-08 01:59:51 +0000

    modify top_rated_checkins to (optionally) IGNORE_REPEAT_BEERS

--- scripts/top_rated_checkins.py
+++ scripts/top_rated_checkins.py
@@ -5,21 +5,28 @@ import untappd
 
 CHECKINS = untappd.load_latest_checkins()
 
-times45 = [
-    ci.datetime
-    for ci in CHECKINS
-    if ci.rating == 4.5
-]
+IGNORE_REPEAT_BEERS = True
 
-times425 = [
-    ci.datetime
-    for ci in CHECKINS
-    if ci.rating == 4.25
-]
+hit_beers = set()
+datetimes_by_rating = {
+    4.5: [],
+    4.25: [],
+}
 
+for ci in CHECKINS:
+    if ci.beer in hit_beers:
+        continue
+    if ci.rating in datetimes_by_rating:
+        datetimes_by_rating[ci.rating].append(ci.datetime)
+        # todo :: behaviour for 4.25->4.5 and 4.5->4.25?
+        if IGNORE_REPEAT_BEERS:
+            hit_beers.add(ci.beer)
+
+
+# todo :: consider merge with cumulative_checkins?
 seaborn.set()
 plt.figure(figsize=(12.8, 7.2))
-plt.plot(times45, range(len(times45)), label="4.5 ratings")
-plt.plot(times425, range(len(times425)), label="4.25 ratings")
+for rating, datetimes in datetimes_by_rating.items():
+    plt.plot(datetimes, range(len(datetimes)), label=f"{rating} ratings")
 plt.legend()
 plt.show()
