66fd5ae77933f6ea96bd96a3bf3fc780154c94b4
Indivicivet <indivicivet@gmail.com>
2023-03-01 14:05:42 +0000

    add top_beers_normalized_by_abv

---
+++ scripts/top_beers_normalized_by_abv.py
@@ -0,0 +1,68 @@
+"""
+initially a copy-paste of top_beers_normalized_by_style_category.py
+"""
+# todo :: combine with ratings_vs_abv_stats?
+# todo :: consolidate with top_checkins_normalized_by_time_window.py
+# and top_beers_normalized_by_style category
+# (although need to think about that being checkins, and this being beers...)
+# todo :: figure out better normalization (by multiple params?)
+
+import statistics
+from collections import defaultdict
+
+import numpy as np
+
+import untappd
+import untappd_utils
+
+CHECKINS = untappd.load_latest_checkins()
+
+
+# LOWER_BOUNDS + rounding copied from rating_vs_abv_stats.py;
+# todo :: think how to consolidate
+LOWER_BOUNDS = np.linspace(0, 14.5, 21)
+
+def round_abv(abv):
+    try:
+        return next(x for x in LOWER_BOUNDS if x >= abv)
+    except StopIteration:
+        return LOWER_BOUNDS[-1]
+
+
+all_mean, all_std = untappd_utils.mean_and_std(CHECKINS)
+
+# calculate statistics to normalize against
+# note: compared to normalized by time, here we go by beer (not by checkin)
+# making it different just for fun :) (and here I think it's slightly better)
+ratings_by_beer = untappd.average_rating_by_beer(CHECKINS)
+ratings_by_abv = {x: [] for x in LOWER_BOUNDS}
+for beer, rating in ratings_by_beer.items():
+    ratings_by_abv[round_abv(beer.abv)].append(rating)
+
+stats_by_abv = {
+    abv: (statistics.mean(ratings), statistics.stdev(ratings))
+    for abv, ratings in ratings_by_abv.items()
+}
+
+
+for beer, rating in ratings_by_beer.items():
+    abv_mean, abv_std = stats_by_abv[round_abv(beer.abv)]
+    beer._normalized_rating = (
+        all_mean + all_std * (rating - abv_mean) / abv_std
+    )
+
+print(f"beers normalized by abv bracket statistics")
+print()
+beers_sorted = sorted(
+    ratings_by_beer,
+    key=lambda beer: beer._normalized_rating,
+    reverse=True,
+)
+for i, beer in enumerate(beers_sorted[:20]):
+    rating = ratings_by_beer[beer]
+    print(
+        f"#{i + 1}, rating {rating:.3f} normalized {beer._normalized_rating:.3f}"
+        f" with abv {beer.abv}% (in bracket {round_abv(beer.abv):.3f}%+)"
+    )
+    print(beer)
+    print()
