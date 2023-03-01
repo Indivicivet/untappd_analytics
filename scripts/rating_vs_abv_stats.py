25d1e45cbec5eb60b8a7cdf5444d757734e2486d
Indivicivet <indivicivet@gmail.com>
2023-03-01 13:37:01 +0000

    add rating_vs_abv_stats

---
+++ scripts/rating_vs_abv_stats.py
@@ -0,0 +1,34 @@
+import statistics
+from collections import defaultdict
+
+from matplotlib import pyplot as plt
+
+import untappd
+import untappd_utils
+
+LOWER_BOUNDS = list(range(15))
+
+CHECKINS = untappd.load_latest_checkins()
+
+ratings_by_bucket = {
+    x: []
+    for x in LOWER_BOUNDS
+}
+for ci in CHECKINS:
+    try:
+        lower_bound = next(x for x in LOWER_BOUNDS if x >= ci.beer.abv)
+    except StopIteration:
+        lower_bound = LOWER_BOUNDS[-1]
+    ratings_by_bucket[lower_bound].append(ci)
+
+
+things_to_plot = zip(*[
+    untappd_utils.mean_plus_minus_std(cis)
+    for cis in ratings_by_bucket.values()
+])
+plt.figure(figsize=(12.8, 7.2))
+for thing, label in zip(things_to_plot, ["mean - 1std", "mean", "mean + 1std"]):
+    print(thing)
+    plt.plot(ratings_by_bucket.keys(), thing, label=label)
+plt.legend()
+plt.show()
