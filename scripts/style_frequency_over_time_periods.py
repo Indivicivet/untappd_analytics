e93a981fc3e4689f52c66bc5a9b4e229c36d9827
Indivicivet <indivicivet@gmail.com>
2023-03-02 13:46:00 +0000

    add PERCENTAGES option to style_frequency_over_time_periods

--- scripts/style_frequency_over_time_periods.py
+++ scripts/style_frequency_over_time_periods.py
@@ -9,6 +9,9 @@ import untappd
 CHECKINS = untappd.load_latest_checkins()
 
 GROUP_TIMESPAN = datetime.timedelta(days=31 * 4)
+
+PERCENTAGES = False
+
 start_date = min(c.datetime for c in CHECKINS)
 end_date = max(c.datetime for c in CHECKINS) - GROUP_TIMESPAN
 day_starts = [
@@ -22,6 +25,7 @@ for start in day_starts:
     for ci in CHECKINS:
         if start <= ci.datetime < start + GROUP_TIMESPAN:
             counts_this_time[ci.beer.get_style_category()] += 1
+            counts_this_time["total"] += 1
     category_counts.append(counts_this_time)
 
 seaborn.set()
@@ -30,11 +34,18 @@ plt.figure(figsize=(12.8, 7.2))
 for category in untappd.CATEGORY_KEYWORDS:
     plt.plot(
         day_starts,
-        [counts[category] for counts in category_counts],
+        [
+            counts[category] * (
+                100 / counts["total"]
+                if PERCENTAGES
+                else 1
+            )
+            for counts in category_counts
+        ],
         label=category,
     )
 plt.title(f"checkins by style ({GROUP_TIMESPAN.days} days following date)")
 plt.xlabel("date")
-plt.ylabel("checkins")
+plt.ylabel("checkin %" if PERCENTAGES else "checkins")
 plt.legend()
 plt.show()
