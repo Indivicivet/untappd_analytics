d9d15594055ef9e7ea66a3b7001ac0a416ca9dc0
indi <indivicivet@gmail.com>
2023-10-11 01:20:49 +0100

    print list of top checkins by semantic compound score

--- scripts/sentiments_nltk_vader.py
+++ scripts/sentiments_nltk_vader.py
@@ -10,11 +10,17 @@ import untappd
 CIS = untappd.load_latest_checkins()
 
 analyzer = vader.SentimentIntensityAnalyzer()
+for c in CIS:
+    c._compound_score = analyzer.polarity_scores(c.comment)["compound"]
+
+
+for c in sorted(CIS, key=lambda x: x._compound_score, reverse=True)[:25]:
+    print(f"{c._compound_score} ({c.rating}) | {c.beer.name} | {c.comment}")
 
 plt.figure(figsize=(12.8, 7.2))
 plt.scatter(
     [c.rating for c in CIS],
-    [analyzer.polarity_scores(c.comment)["compound"] for c in CIS],
+    [c._compound_score for c in CIS],
     alpha=0.05,
     s=100,
 )
