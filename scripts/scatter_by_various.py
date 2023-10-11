360f79f3482e25053c482659ef516856d642b8f3
indi <indivicivet@gmail.com>
2023-10-11 01:02:30 +0100

    add sentiment analysis to scatter_by_various.py

--- scripts/scatter_by_various.py
+++ scripts/scatter_by_various.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+from nltk.sentiment import vader
 
 import untappd
 import untappd_utils
@@ -28,14 +29,18 @@ def scatter_things(
 def save_various_scatters(checkins, out_dir=None):
     if out_dir is None:
         out_dir = Path(__file__).parent / "out"
+    analyzer = vader.SentimentIntensityAnalyzer()
+    sentiments = [analyzer.polarity_scores(c.comment) for c in checkins]
     dict_of_things = {
-        "ratings": [c.rating for c in checkins],
+        "rating": [c.rating for c in checkins],
         "comment_length": [len(c.comment) for c in checkins],
-        "time_of_day": [c.datetime.hour + c.datetime.minute / 60 for c in checkins]
+        "time_of_day": [c.datetime.hour + c.datetime.minute / 60 for c in checkins],
+        "sentiment_compound": [s["compound"] for s in sentiments],
     }
     for x, y, kwargs in [
-        ["comment_length", "ratings", {}],
-        ["time_of_day", "comment_length", {}]
+        ["comment_length", "rating", {}],
+        ["time_of_day", "comment_length", {}],
+        ["sentiment_compound", "rating", {}],
     ]:
         scatter_things(
             dict_of_things=dict_of_things,
