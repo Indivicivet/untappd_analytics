d4c0451c87025464926b1dc0dc6b1e699c554ff6
indi <indivicivet@gmail.com>
2025-01-17 00:36:11 +0000

    format top venue repeats

--- scripts/top_venue_repeats.py
+++ scripts/top_venue_repeats.py
@@ -12,10 +12,12 @@ places = defaultdict(Counter)
 for ci in CIS:
     places[ci.venue][ci.beer] += 1
 
-print(
-    sorted(
-        places.items(),
-        key=lambda t: sum(t[1].values()),
-        reverse=True,
-    )[:20]
-)
+for venue_i, (venue, beer_counts) in enumerate(sorted(
+    places.items(),
+    key=lambda t: sum(t[1].values()),
+    reverse=True,
+)[:10]):
+    print(f"#{venue_i + 1}", "No venue" if venue is None else venue.name)
+    for beer_i, (beer, count) in enumerate(beer_counts.most_common(10)):
+        print(f"#{beer_i + 1} ({count}): {beer}")
+    print()
