ecdd7734cd221a2ae5e4fa2519ee8e9d6fa498dd
indi <indivicivet@gmail.com>
2025-03-25 23:47:03 +0000

    better attempt at layout ^^

--- scripts/venues_by_time.py
+++ scripts/venues_by_time.py
@@ -20,12 +20,14 @@ day_counts = defaultdict(lambda: Counter())
 for ci in CIS:
     day_counts[ci.datetime.strftime("%A")][ci.venue] += 1
 
-_, axes = plt.subplots(1, 7, figsize=(12.8, 7.2))
+ROWS = 2
+COLS = 4
+_, axes = plt.subplots(ROWS, COLS, figsize=(12.8, 7.2))
 for i, day in enumerate(DAYS):
     top_counts = day_counts[day].most_common(10)
-    axes[i].pie(
+    axes[i // COLS, i % COLS].pie(
         [c for _, c in top_counts],
         labels=[v for v, _ in top_counts],
     )
-    #axes[i].legend()
+    # axes[i // COLS, i % COLS].legend()
 plt.show()
