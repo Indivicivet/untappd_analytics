2ef1e176c94a56229fc2e690430ae4e90d3b7280
indi <indivicivet@gmail.com>
2025-07-19 00:39:16 +0100

    smaller title, bigger plot

--- scripts/festival_home_country.py
+++ scripts/festival_home_country.py
@@ -22,14 +22,14 @@ cols = math.ceil(len(festivals) ** 0.5)
 fig, axs = plt.subplots(
     math.ceil(len(festivals) / cols),
     cols,
-    figsize=(12.8, 7.2),
+    figsize=(12.8, 9),
 )
 for ax in axs.flat:
     ax.set_visible(False)
 for ax, (festival_name, festival_data) in zip(axs.flat, festivals.items()):
     top_country, top_count = festival_data.most_common(1)[0]
     rest = festival_data.total() - top_count
-    ax.set_title(festival_name)
+    ax.set_title(festival_name, fontsize=10)
     ax.pie(
         [top_count, rest],
         labels=[top_country, "other" if rest else ""],
