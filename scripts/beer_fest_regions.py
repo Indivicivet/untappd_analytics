d11ecfb6d2d984b9ac911c588178cf6c06dacf12
indi <indivicivet@gmail.com>
2025-11-14 00:14:07 +0000

    include pie chart for frequencies

--- scripts/beer_fest_regions.py
+++ scripts/beer_fest_regions.py
@@ -21,16 +21,26 @@ country_freq = Counter(
     for ci in FEST_CIS
 )
 print(country_freq.most_common())  # todo :: pie
+
 country_order = [c for c, _ in country_freq.most_common()][::-1]  # top to bottom
+
+fig, (ax_pie, ax_violin) = plt.subplots(1, 2, figsize=(12, 7.2))
+
+ax_pie.pie(
+    [country_freq[c] for c in country_order],
+    labels=country_order,
+    startangle=90,
+)
+
 # taken from rating_histogram_by_various.show_violin()
-plt.violinplot(
+ax_violin.violinplot(
     list([ratings_by_country[country] for country in country_order]),
     quantiles=[[0, 0.1, 0.5, 0.9, 1]] * len(country_order),
     vert=False,
     widths=0.75,
     bw_method=0.25 + 0.1,  # known discrete sampling, smoothed a bit extra
 )
-plt.gca().set_yticks(
+ax_violin.set_yticks(
     list(range(1, len(country_order) + 1)),
     labels=list(country_order),
 )
